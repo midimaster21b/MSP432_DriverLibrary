f7e774b33cf654f32a55520006a16276014ca4c1
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-07-10 22:35:52 -0400

    Update main to reflect currently available headers and tests

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include "jedge_uart.h"
 #include "jedge_spi.h"
 #include "jedge_systick.h"
+#include "jedge_clocks.h"
+#include "jedge_sccb.h"
+#include "jedge_ov7670.h"
+#include "jedge_sd_card.h"
 
 /**
  * main.c
@@ -13,13 +17,24 @@ void main(void)
   WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
 
   // UART Test
-  UART_test();
+  /* UART_test(); */
 
   // SPI Test
-  SPI_test();
+  /* SPI_test(); */
 
   // SysTick Test
-  systick_test();
+  /* systick_test(); */
+
+  init_clocks();
+
+  // SCCB test
+  /* sccb_test(); */
+
+  // OV7670 test
+  /* ov7670_test(); */
+
+  // SD card test
+  /* sd_test(); */
 
   while(1) {
   }
